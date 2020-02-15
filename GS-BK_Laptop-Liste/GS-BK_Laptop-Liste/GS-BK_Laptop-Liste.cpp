102e8cc761120e53ce73d15ad63cb3b93203d23e
René Lohrer <rene.lohrer@de.kaercher.com>
2020-02-15 13:29:46 +0100

    added functions of the project added Header

--- GS-BK_Laptop-Liste/GS-BK_Laptop-Liste/GS-BK_Laptop-Liste.cpp
+++ GS-BK_Laptop-Liste/GS-BK_Laptop-Liste/GS-BK_Laptop-Liste.cpp
@@ -11,6 +11,15 @@
 							Hierbei ist es möglich Laptops anzulegen und zu "buchen".
 							Die Idee kam mir da wir im Unternehmen eine Excel Liste haben die genau dies tut,
 							hierbei wird es den Auszubildenden ohne Laptop ermöglicht einen Laptop auszuleihen.
+		Funtkionalitäten der Anwendung:
+									  - Hinzufügen von Laptops
+									  - Hinzufügen von Zubehör
+									  - Buchen der Laptops
+									  - Buchungen Stornieren
+									  - Ausgeben aller Laptops mit dem Aktuellen Status
+									  - Suchen eines Laptops / Anzeige eines Laptops mit Status
+									  - Entfernen von Laptops
+									  - Entfernen von Zubehör
 		Source Code und weitere Informationen:
 											  https://github.com/TheShadowPlayer/GS-BK_Laptop-Liste/
 */
@@ -19,12 +28,12 @@
 /*
 Einbinden von Systembibliotheken
 Folgende werden verwendet:
+	Name der Bibliothek | Beschreibung was diese macht
 	- pch.h | Hier werden "pre-compiled headers" in das Program Eingebunden
 	- iostream | Hier sind die Ein- und Ausgabe-Funktionen von C++ enthalten
 	- cmath | Fügt mehrere Datentypen der Standartbibliothek ein
 	- string.h | Fügt verschiedene Mehtoden für "string" ein
 	- using namespace std | Hier werden die befehle vereinfacht, das man statt std::cout nur noch cout machen muss
-
 */
 #include "pch.h"
 #include <iostream>
@@ -32,10 +41,56 @@ Folgende werden verwendet:
 #include "string.h"
 using namespace std;
 
+/*
+	Deklarationen von Methoden
+
+	Name der Methode | Funktion der Methode
+	- AusgabeHeader | Hier wird die Ausgabe des Standart Header der Anwendung 
+	- AusgabeMenue | Hier wird die Ausgabe des Hauptmenues 
+*/
+
+void AusgabeHeader();
+void AusgabeMenue();
+
+
+/*
+	Daklaration von Klassen
+
+	Name der Klasse | Beschreibung der Klasse
+*/
+
+
+
+/*
+
+	Die main() Methode ist der Einstiegspunkt für das Programm
+
+*/
 int main()
 {
-	cout << "Hello World!\n";
+	AusgabeHeader();
+	AusgabeMenue();
 
-	system("PAUSE");
 	return 0;
+}
+
+/*
+
+	Definition der Methode
+
+*/
+
+void AusgabeHeader()
+{
+	cout << "+-----------------------------------------+" << endl;
+	cout << "|                                         |" << endl;
+	cout << "|    L A P T O P  -                       |" << endl;
+	cout << "|                   V E R M I E T U N G   |" << endl;
+	cout << "|                                         |" << endl;
+	cout << "+-----------------------------------------+" << endl;
+}
+
+void AusgabeMenue()
+{
+
 }
