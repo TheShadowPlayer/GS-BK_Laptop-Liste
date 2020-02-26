adecb90210a7b9a8ff235570d3cacdfb77e8b20a
René Lohrer <rene@lohrer-home.de>
2020-02-26 21:12:46 +0100

    Updated Git Ignore added first Objects

--- GS-BK_Laptop-Liste/GS-BK_Laptop-Liste/GS-BK_Laptop-Liste.cpp
+++ GS-BK_Laptop-Liste/GS-BK_Laptop-Liste/GS-BK_Laptop-Liste.cpp
@@ -5,7 +5,7 @@
 		Fachrichtung: SAE
 		Entwicklungsumgebungen:
 								Visual Studio 2017
-								Visual Studio Code
+								Visual Studio 2019
 		Kurze Beschreibung:
 							Dies ist ein kleines Projeckt bei dem es sich um eine Art Laptop Buchungs Software geht.
 							Hierbei ist es möglich Laptops anzulegen und zu "buchen".
@@ -38,6 +38,7 @@ Folgende werden verwendet:
 #include "pch.h"
 #include <iostream>
 #include <cmath>
+#include <time.h>
 #include "string.h"
 using namespace std;
 
@@ -45,8 +46,8 @@ using namespace std;
 	Deklarationen von Methoden
 
 	Name der Methode | Funktion der Methode
-	- AusgabeHeader | Hier wird die Ausgabe des Standart Header der Anwendung 
-	- AusgabeMenue | Hier wird die Ausgabe des Hauptmenues 
+	- AusgabeHeader | Hier wird die Ausgabe des Standart Header der Anwendung
+	- AusgabeMenue | Hier wird die Ausgabe des Hauptmenues
 */
 
 void AusgabeHeader();
@@ -59,6 +60,38 @@ void AusgabeMenue();
 	Name der Klasse | Beschreibung der Klasse
 */
 
+class Objekt {
+private:
+	char name[20];
+	int type;
+	char bucher[20];
+	bool gebucht;
+protected:
+
+
+	char* getName() { return name; };
+	int getType() { return type; };
+	char* getBucher() { return bucher; };
+	bool getStatus() { return gebucht; };
+public:
+	Objekt(char name[], int type);
+	virtual void print() {};
+	bool buchen(char bucher[10]);
+};
+
+class Laptop : public Objekt {
+private:
+	char hersteller[20];
+	char model[20];
+	int prozessorKerne;
+	int prozessorThreads;
+	double prozessorTakt;
+	double arbeitsspeicher;
+public:
+	Laptop(char name[], char hersteller[], char model[], int prozessorKerne, int prozessorThreads, double prozessorTakt, double arbeitsspeicher);
+	~Laptop();
+	void print();
+};
 
 
 /*
@@ -70,7 +103,12 @@ int main()
 {
 	AusgabeHeader();
 	AusgabeMenue();
-
+	char test[] = "test";
+	char hp[] = "HP";
+	char model[] = "ZBook 1";
+	Objekt* testObj = new Laptop(test, hp, model, 1000, 696969, 69.69, 187.69);
+	testObj->print();
+	system("PAUSE");
 	return 0;
 }
 
@@ -93,4 +131,42 @@ void AusgabeHeader()
 void AusgabeMenue()
 {
 
+}
+
+Objekt::Objekt(char name[], int type)
+{
+	strcpy_s(Objekt::name, name);
+	Objekt::type = type;
+	strcpy_s(Objekt::bucher, "");
+	Objekt::gebucht = false;
+}
+
+bool Objekt::buchen(char bucher[10])
+{
+	if (!Objekt::gebucht) {
+		strcpy_s(Objekt::bucher, bucher);
+		Objekt::gebucht = true;
+		return true;
+	}
+	return false;
+}
+
+Laptop::Laptop(char name[], char hersteller[], char model[], int prozessorKerne, int prozessorThreads, double prozessorTakt, double arbeitsspeicher) : Objekt(name, 1)
+{
+	strcpy_s(Laptop::hersteller, hersteller);
+	strcpy_s(Laptop::model, model);
+	Laptop::prozessorKerne = prozessorKerne;
+	Laptop::prozessorThreads = prozessorThreads;
+	Laptop::prozessorTakt = prozessorTakt;
+	Laptop::arbeitsspeicher = arbeitsspeicher;
+}
+
+Laptop::~Laptop()
+{
+	cout << "Wurde gelöscht";
+}
+
+void Laptop::print()
+{
+	cout << Laptop::getType() << " - " << Laptop::getName() << " - [" << Laptop::hersteller << ", " << Laptop::model << ", " << Laptop::prozessorKerne << ", " << Laptop::prozessorThreads << ", " << Laptop::prozessorTakt << ", " << Laptop::arbeitsspeicher << "]" << endl;
 }
